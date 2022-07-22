cb89164d1632b649c0144935df0e110ca5d077ca
atinn <tinateen584@gmail.com>
2022-07-22 15:59:09 +0700

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 import image  from '../images/viktor-forgacs-B88PgQXS4qg-unsplash.jpg'
 const client_id = 'c2aed3f04c3f4851a294ab44fab9feee'
 const statee = 'scope=streaming user-read-private user-read-recently-played user-read-playback-state user-library-read playlist-modify-private playlist-read-collaborative user-read-email playlist-read-private user-library-modify user-read-playback-state user-modify-playback-state'
-const url_spotify = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost:3000/&state=${statee}`
+const url_spotify = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost:3000&state=${statee}`
 
 export default function Login() {
   return (
