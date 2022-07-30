af88d63c7a02c8e05333643869affd12a4bf590d
atinn <tinateen584@gmail.com>
2022-07-30 10:10:32 +0700

    change

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,14 @@ import React from 'react'
 import image  from '../images/viktor-forgacs-B88PgQXS4qg-unsplash.jpg'
 const client_id = 'c2aed3f04c3f4851a294ab44fab9feee'
 const statee = 'scope=streaming user-read-private user-read-recently-played user-read-playback-state user-library-read playlist-modify-private playlist-read-collaborative user-read-email playlist-read-private user-library-modify user-read-playback-state user-modify-playback-state'
-const url_spotify = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost:3000&state=${statee}`
+const redirect_uri = "https://spotifyme-6970f.web.app"
+
+const url_spotify = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&state=${statee}`
 
 export default function Login() {
   return (
     <div className='halamanLogin' style={{backgroundImage:`url(${image})`,backgroundPosition:'center',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
-        <div className='login'>
+        <div className='login'>  
         <a href={url_spotify} className='log'>Login</a> 
         </div>
     </div>
